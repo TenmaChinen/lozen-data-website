b214b039fbe3aa59edc4cb0b2c53b14859f5c3be
TenmaMune <tenmachinen@gmail.com>
2023-04-27 19:18:44 +0200

    Empty uri to redirect trainings list

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
+from django.views.generic import RedirectView
 from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='trainings/list/')),
     path('trainings/', include('trainings.urls')),
     path('programs/', include('programs.urls')),
     path('exercises/', include('exercises.urls')),
